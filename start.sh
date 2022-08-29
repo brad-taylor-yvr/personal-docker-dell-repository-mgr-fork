3b85138947fa0106fbb8bcc4642f1cb0113bab0d
Brad Taylor <toddles_decoys0v@icloud.com>
2022-08-28 21:04:36 -0700

    disable socat in start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,9 @@
 /opt/dell/dellemcrepositorymanager/DRM_Service.sh &
 
 # DRM binds to localhost port 8090 inside container and socat is needed to make it accessible
-socat tcp-listen:8091,reuseaddr,fork tcp:127.0.0.1:8090
+#socat tcp-listen:8091,reuseaddr,fork tcp:127.0.0.1:8090
+
+# this is what it looks like with version 3.4.2:
+# [drmuser@ubuntu02_drm /]$ netstat -an | grep LISTEN | grep 8091
+# tcp        0      0 0.0.0.0:8091            0.0.0.0:*               LISTEN 
+# listening on every IP, not just localhost
